e57b6c8ab8bc6728962e83d065ddd35a2ea15e18
thekashfi <alikashfi12345@gmail.com>
2022-08-25 08:24:07 +0430

    site title

--- resources/views/site/layout/header.blade.php
+++ resources/views/site/layout/header.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Ajerni @hasSection('title') - @yield('title') @endif </title>
+    <title>{{ __('PageTitle') }} @hasSection('title') - @yield('title') @endif </title>
     <link rel="icon" type="image/png" href="{{ asset('images/main/favicon.png') }}">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
