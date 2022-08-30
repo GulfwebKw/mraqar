ca41da0a1703d70f9cbc2cb7b12264e03253d138
thekashfi <alikashfi12345@gmail.com>
2022-08-30 13:01:07 +0430

    count buy package in payment result

--- resources/views/site/pages/payment.blade.php
+++ resources/views/site/pages/payment.blade.php
@@ -7,13 +7,24 @@
         <div class="row justify-content-center">
             <div class="mdc-card" style="padding: 15px; min-width: 50%;">
                 <div class="card-body mdc-card__action--button">
-                    <h4 class="card-title">{{optional($payment->package)->title_en}}</h4>
+                    <h4 class="card-title">{{$payment->packageHistory->count >= 2 ? "{$payment->packageHistory->count} x " : '' }}{{optional($payment->package)['title_' . app()->getLocale()]}}</h4>
 
                     <p class="card-text">
-                        {{__('price')}}: {{number_format(optional($payment)->price , env('NUMFORMAT' , 0 ))}} {{__('kd_title')}}
+                        @if(optional($payment)->packageHistory->count >= 2)
+                            {{__('price')}}: {{number_format(optional($payment)->price , env('NUMFORMAT' , 0 ))}} {{__('kd_title')}}
+                            ({{optional($payment)->packageHistory->count}} x {{number_format(optional($payment)->packageHistory->price , env('NUMFORMAT' , 0 ))}} {{__('kd_title')}})
+                        @else
+                            {{__('price')}}: {{number_format(optional($payment)->price , env('NUMFORMAT' , 0 ))}} {{__('kd_title')}}
+                        @endif
                     </p>
                     <p class="card-text">
                         {{__('result')}}: <strong  @if($message=="CAPTURED")style="color:green;"  @else style="color:red;" @endif > {{$message}}</strong>
+                    </p>
+                    <p class="card-text">
+                        {{__('number_of_normal_ads')}}: {{optional($payment)->packageHistory->count_advertising}}
+                    </p>
+                    <p class="card-text">
+                        {{__('number_of_premium_ads')}}: {{optional($payment)->packageHistory->count_premium}}
                     </p>
                     <h4 class="card-title">{{ __('paymentdetails') }}</h4>
                     <p class="card-text">
